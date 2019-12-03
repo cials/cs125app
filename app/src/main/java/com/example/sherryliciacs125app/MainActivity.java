53d1beacd46296e6f57993951db472a2c1d042df
siyu wu <siyuwu4@illinois.edu>
2019-12-03 00:31:31 -0600

    file for marie to download

--- app/src/main/java/com/example/sherryliciacs125app/MainActivity.java
+++ app/src/main/java/com/example/sherryliciacs125app/MainActivity.java
@@ -13,16 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button lipstick = findViewById(R.id.lipstick);
-        lipstick.setOnClickListener(unused -> startActivity(new Intent(this, Lipstick.class)));
-
-        Button lipgloss = findViewById(R.id.lipgloss);
-        lipgloss.setOnClickListener(unused -> startActivity(new Intent(this, Lipgloss.class)));
-
-        Button lipstain = findViewById(R.id.lipstain);
-        lipstain.setOnClickListener(unused -> startActivity(new Intent(this, Lipstain.class)));
-
-        Button lipliquid = findViewById(R.id.lipliquid);
-        lipliquid.setOnClickListener(unused -> startActivity(new Intent(this, Lipliquid.class)));
+//        Button lipstick = findViewById(R.id.lipstick);
+//        lipstick.setOnClickListener(unused -> startActivity(new Intent(this, Lipstick.class)));
+//
+//        Button lipgloss = findViewById(R.id.lipgloss);
+//        lipgloss.setOnClickListener(unused -> startActivity(new Intent(this, Lipgloss.class)));
+//
+//        Button lipstain = findViewById(R.id.lipstain);
+//        lipstain.setOnClickListener(unused -> startActivity(new Intent(this, Lipstain.class)));
+//
+//        Button lipliquid = findViewById(R.id.lipliquid);
+//        lipliquid.setOnClickListener(unused -> startActivity(new Intent(this, Lipliquid.class)));
     }
 }
